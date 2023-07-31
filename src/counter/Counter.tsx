4ebdb6a3a036325cd1bc5d3d17a6a7b6d7ee909c
BYAsurim <Pers0naL276276@gmail.com>
2023-07-31 23:08:13 +0300

    added styles for counter

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 import s from './Counter.module.css';
+import {Button, createTheme, ThemeProvider} from "@mui/material";
 
 
 export function Counter() {
 
     let [number, setNumber] = useState<number>(0)
     let [error, setError] = useState<string>('')
-
     const incHandler = () => {
         setNumber(++number)
 
@@ -20,20 +20,39 @@ export function Counter() {
     }
 
 
-    return (<div className={s.header}>
-            <div className={s.number}>
-                <div className={error === 'error' ? s.error : ''}>
-                    {
-                        number
-                    }
+    return (
+        <div className={s.main}>
+            <div className={s.header}>
+                <div className={s.number}>
+                    <div className={error === 'error' ? s.error : ''}>
+                        {
+                            number
+                        }
+                    </div>
                 </div>
-            </div>
-            <div className={s.buttons}>
-                <button onClick={incHandler} disabled={number === 5}>inc
-                </button>
-                <button onClick={resetHandler} disabled={number === 0}>reset
-                </button>
+                <div className={s.buttons}>
+                    <ThemeProvider theme={theme}>
+                        <div>
+                            <Button size={"small"} variant="outlined" onClick={incHandler}
+                                    disabled={number === 5}>inc</Button>
+                        </div>
+                        {/*<button onClick={incHandler} disabled={number === 5}>inc*/}
+                        {/*</button>*/}
+                        <div>
+                            <Button size={"small"} variant="outlined" onClick={resetHandler}
+                                    disabled={number === 0}>reset</Button>
+                        </div>
+                        {/*<button onClick={resetHandler} disabled={number === 0}>reset*/}
+                        {/*</button>*/}
+                    </ThemeProvider>
+                </div>
             </div>
         </div>
     )
 }
+
+const theme = createTheme({
+    typography: {
+        fontSize: 10,
+    },
+});
